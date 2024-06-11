5400ae0b8e81cea1cec0a88925e80c4a3227d141
IamDruid84 <druidis4e@mail.ru>
2024-06-11 12:26:52 +0300

    Update mylib.go

--- mylib.go
+++ mylib.go
@@ -1,9 +1,12 @@
 package myLib
 
+import "github.com/IamDruid84/myLib"
+
 func main() {
 	DoSomething(0, 1)
 }
 
 func DoSomething(do int, do2 int) int {
+	myLib.DoSomething(1, 2)
 	return do2 + do
 }
